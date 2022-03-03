02ac6eb51266e552bd33db3a51f1b1fe811fe8e8
Alessio Caruso <caruso.alessio1999@gmail.com>
2022-03-03 16:13:02 +0100

    exercise completed, code to be revised

--- js/script.js
+++ js/script.js
@@ -12,18 +12,71 @@ document.querySelector('.palindrome .btn').addEventListener('click', function ()
   for (let i = 0; i < userWord.length; i++) {
     reverseWord+=words[i];
   }
+  document.querySelector('.palindrome h2').classList.remove("invisible")
   if (palindrome(userWord , reverseWord)) {
-    document.querySelector('.palindrome h2').innerHTML=`La tua parola &egrave; palindroma`
+    document.querySelector('.palindrome span').innerHTML=` &egrave; palindroma`
   } 
   else{
-    document.querySelector('.palindrome h2').innerHTML=`La tua parola non &egrave; palindroma`
+    document.querySelector('.palindrome span').innerHTML=` non &egrave; palindroma`
   }
-})
-
+});
 
 function palindrome(tempWord , tempReversWord)
 {
   if (tempWord == tempReversWord) {
+    return true;
+  }
+  return false;
+}
+
+// part of even and odd
+let userNumber=0;
+let numberBot=0;
+console.log(numberBot);
+document.getElementById('even').addEventListener('click', function(){
+  userNumber=parseInt(document.querySelector('.even-odd input').value)
+  numberBot=numberRandom();
+  document.querySelector(".even-odd span").innerHTML=(numberBot);
+  let sumNumbers=sum(userNumber, numberBot);
+  if (isEven(sumNumbers)) {
+    console.log('hai vinto');
+    document.querySelector(".even-odd h3").innerHTML=('Hai vinto')
+  }
+  else{
+    console.log("hai perso");
+    document.querySelector(".even-odd h3").innerHTML=('Hai perso')
+  }
+  console.log(sumNumbers);
+
+
+});
+document.getElementById('odd').addEventListener('click', function(){
+  userNumber=parseInt(document.querySelector('.even-odd input').value)
+  numberBot=numberRandom();
+  document.querySelector(".even-odd span").innerHTML=(numberBot);
+  let sumNumbers=sum(userNumber, numberBot);
+  if (isEven(sumNumbers)) {
+    console.log("hai perso");
+    document.querySelector(".even-odd h3").innerHTML=('Hai perso')
+  }
+  else{
+    console.log('hai vinto');
+    document.querySelector(".even-odd h3").innerHTML=('Hai vinto')
+  }
+  console.log(sumNumbers);
+});
+
+// function that generates a number from 1 to 5
+function numberRandom(){
+  let tempnumb=Math.floor((Math.random()*5)+1);
+  return tempnumb;
+}
+function sum(numb1, numb2){
+  numb1+=numb2;
+  return numb1;
+}
+function isEven(number){
+  if(number % 2 === 0){
     return true;
   }
   return false;
