4726ead45fad2fe0ec64b6f2db3db61a3eea5804
Alessio Caruso <caruso.alessio1999@gmail.com>
2022-03-03 16:52:21 +0100

    added comments and conditions, exercise completed

--- js/script.js
+++ js/script.js
@@ -30,54 +30,72 @@ function palindrome(tempWord , tempReversWord)
 }
 
 // part of even and odd
+
 let userNumber=0;
 let numberBot=0;
-console.log(numberBot);
+// added function to even button
 document.getElementById('even').addEventListener('click', function(){
   userNumber=parseInt(document.querySelector('.even-odd input').value)
-  numberBot=numberRandom();
-  document.querySelector(".even-odd span").innerHTML=(numberBot);
-  let sumNumbers=sum(userNumber, numberBot);
-  if (isEven(sumNumbers)) {
-    console.log('hai vinto');
-    document.querySelector(".even-odd h3").innerHTML=('Hai vinto')
+  if(isNaN(userNumber) || userNumber >5 || userNumber<1){
+    document.querySelector(".even-odd h3").innerHTML=('Devi inserire un numero da 1 a 5!!')
   }
   else{
-    console.log("hai perso");
-    document.querySelector(".even-odd h3").innerHTML=('Hai perso')
+    numberBot=numberRandom();
+    returnNumberbots();
+    let sumNumbers=sum(userNumber, numberBot);
+    if (isEven(sumNumbers)) {
+      returnWin();
+    }
+    else{
+      returnLose()
+    }
   }
-  console.log(sumNumbers);
-
-
 });
+// added function to odd button
 document.getElementById('odd').addEventListener('click', function(){
   userNumber=parseInt(document.querySelector('.even-odd input').value)
-  numberBot=numberRandom();
-  document.querySelector(".even-odd span").innerHTML=(numberBot);
-  let sumNumbers=sum(userNumber, numberBot);
-  if (isEven(sumNumbers)) {
-    console.log("hai perso");
-    document.querySelector(".even-odd h3").innerHTML=('Hai perso')
+  if(isNaN(userNumber) || userNumber >5 || userNumber<1){
+    document.querySelector(".even-odd h3").innerHTML=('Devi inserire un numero da 1 a 5!!')
   }
   else{
-    console.log('hai vinto');
-    document.querySelector(".even-odd h3").innerHTML=('Hai vinto')
+    numberBot=numberRandom();
+    returnNumberbots();
+    let sumNumbers=sum(userNumber, numberBot);
+    if (isEven(sumNumbers)) {
+      returnLose()
+    }
+    else{
+      returnWin();
+    }
   }
-  console.log(sumNumbers);
 });
-
-// function that generates a number from 1 to 5
+// Function of even or odd
+// function that return a number from 1 to 5 a
 function numberRandom(){
   let tempnumb=Math.floor((Math.random()*5)+1);
   return tempnumb;
 }
+// function that returns a sum
 function sum(numb1, numb2){
   numb1+=numb2;
   return numb1;
 }
+// function that returns if it is even
 function isEven(number){
   if(number % 2 === 0){
     return true;
   }
   return false;
+}
+// function that adds a win text to the html
+function returnWin(){
+  document.querySelector(".even-odd h3").innerHTML=('Hai vinto')
+}
+// function that adds a lose text to the html
+function returnLose(){
+  document.querySelector(".even-odd h3").innerHTML=('Hai Perso')
+}
+// function that adds a number bots to the html
+function returnNumberbots(){
+  document.querySelector(".even-odd span").innerHTML=(numberBot);
 }
